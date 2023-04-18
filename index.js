9c2461ebd9982a642da5fa60d57ffd12c95098df
JPG <jpgtv19@gmail.com>
2023-04-18 00:59:30 -0400

    localhost added to allow cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // allow calls from this origin https://church-localizer.vercel.app'
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://church-localizer.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://church-localizer.vercel.app', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
