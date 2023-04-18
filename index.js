00d5958feeedb8cc11ecc1bd840c2b8aa7bb45b7
JPG <jpgtv19@gmail.com>
2023-04-18 00:56:25 -0400

    access origin allowed

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// allow calls from this origin https://church-localizer.vercel.app'
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://church-localizer.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 // Use the body-parser middleware
 app.use(bodyParser.json());
 
