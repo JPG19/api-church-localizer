37a72cd62b47909d36416f5eebd06b915a2e2670
JPG <jpgtv19@gmail.com>
2023-04-17 23:55:13 -0400

    Added express and added update route

--- controllers/churchController.js
+++ controllers/churchController.js
@@ -1,12 +1,12 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
+import { ScanCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 const params = {
   TableName: 'Churches',
 };
 
-// @desc    Gets All Products
-// @route   GET /api/products
+// @desc    Gets All Churches
+// @route   GET /api/churches
 async function getChurches(req, res) {
   try {
     const credentials = {
@@ -29,9 +29,9 @@ async function getChurches(req, res) {
   }
 }
 
-// @desc    Gets Single Product
-// @route   GET /api/product/:id
-async function getChurch(req, res, id) {
+// @desc    Gets Single Church
+// @route   GET /api/churches/:id
+async function getChurch(req, res) {
   try {
     const credentials = {
       region: process.env.AWS_DEFAULT_REGION,
@@ -40,6 +40,8 @@ async function getChurch(req, res, id) {
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       },
     };
+
+    const id = req.url.split('/')[3];
 
     const ddbClient = new DynamoDBClient(credentials);
 
@@ -59,4 +61,50 @@ async function getChurch(req, res, id) {
   }
 }
 
-export { getChurches, getChurch };
+// @desc    Updates Single Church
+// @route   PUT /api/churches/:id
+async function updateChurch(req, res) {
+  try {
+    const credentials = {
+      region: process.env.AWS_DEFAULT_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    };
+
+    const id = req.url.split('/')[3];    
+
+    // get body from request
+    const { reviews } = req.body;
+    
+    const ddbClient = new DynamoDBClient(credentials);
+
+    const data = await ddbClient.send(new ScanCommand(params));
+    const { Items } = data;
+    const foundItem = Items?.find((Item) => Item.ChurchId === id);
+
+    if (!foundItem) {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'Product Not Found' }));
+    }
+
+    await ddbClient.send(
+      new PutCommand({
+        TableName: params.TableName,
+        Item: {
+          ChurchId: id,
+          // Spread values of foundItem
+          ...foundItem,
+          Reviews: reviews,
+      }}),
+    );
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ success: true }));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { getChurches, getChurch, updateChurch };
--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 import http from 'http';
 import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-import { getChurches, getChurch } from './controllers/churchController.js';
+import { getChurches, getChurch, updateChurch } from './controllers/churchController.js';
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/api/churches' && req.method === 'GET') {
-    getChurches(req, res);
-  } else if (req.url.match(/\/api\/churches\/\w+/) && req.method === 'GET') {
-    const id = req.url.split('/')[3];
-    getChurch(req, res, id);
-  }
-});
-
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const app = express();
 
-export default server;
+// Use the body-parser middleware
+app.use(bodyParser.json());
+
+app.get('/api/churches', (req, res) => {
+  getChurches(req, res);
+});
+
+app.get('/api/churches/:id', (req, res) => {
+  getChurch(req, res);
+});
+
+app.put('/api/churches/:id', (req, res) => {
+  updateChurch(req, res);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
