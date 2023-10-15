bff0bd6bc6059be6d9051c3992e029f1138b341f
JPG <jpgtv19@gmail.com>
2023-10-15 11:17:29 -0300

    add header

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // allow calls from this origin https://church-localizer.vercel.app'
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://church-localizer.vercel.app', 'http://localhost:8000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://church-localizer.vercel.app', 'http://localhost:8000', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
